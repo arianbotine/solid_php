6c3c1bcc2ff081bc660a1e23b621b65cf86fca28
Arian Botine <arian.botine@shift.com.br>
2023-02-03 22:27:42 -0300

    Adicionando arquivo para treinar o principio da responsabilidade unica

---
+++ app_carrinho_compras_single_responsibility_principle/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'arian.botine/app_carrinho_compras',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '828e9f7ca64a9d2cc1588dc45700118e8e1ef90c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'arian.botine/app_carrinho_compras' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '828e9f7ca64a9d2cc1588dc45700118e8e1ef90c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
