4611923acfcf4c7012244535b0dc488ddee85e3a
Arian Botine <arian.botine@shift.com.br>
2023-02-23 23:39:17 -0300

    Implementando testes do objeto item

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ baixar phpunit
 php ..\composer.phar require --dev --prefer-dist phpunit/phpunit
 
 --prefer-dist baixa as dependencias do phpunit
+
+exemplo de execução dos testes unitários:
+
+vendor\bin\phpunit.bat test\itemTest.php
--- app_carrinho_compras_single_responsibility_principle/test/itemTest.php
+++ app_carrinho_compras_single_responsibility_principle/test/itemTest.php
@@ -1,10 +1,77 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use src\Item;
 
-class itemTest extends TestCase {
-    public function testEstouFuncionando() {
+class ItemTest extends TestCase {
+    public function testEstadoInicialItem() {
+        $item = new Item();
+
+        $this->assertEquals('', $item->getDescricao());
+        $this->assertEquals(0, $item->getValor());
+    }
+
+    public function testGetESetDescricao() {
+        $descricao = 'Item teste';
+
+        $item = new Item();
+        $item->setDescricao($descricao);
+        $this->assertEquals($descricao, $item->getDescricao());
+    }
+
+    public function testGetESetValor() {
+        $valor = 15.55;
+
+        $item = new Item();
+        $item->setValor($valor);
+        $this->assertEquals($valor, $item->getValor());
+    }
+
+    public function testItemValido() {
+        $descricao = 'Item teste';
+        $valor = 15.55;
+
+
+        $item = new Item();
+        $item->setValor($valor);
+        $item->setDescricao($descricao);
+        $this->assertTrue($item->itemValido());
+
+        $item->setValor(-15.52);
+        $item->setDescricao($descricao);
+        $this->assertFalse($item->itemValido());
+    }
+
+    public function testItemInvalidoQuandoValorMenorOuIgualAZero() {
+        $descricao = 'Item teste';
+        $valor = -15.55;
+
+        $item = new Item();
+        $item->setValor($valor);
+        $item->setDescricao($descricao);
+        $this->assertFalse($item->itemValido());
+
+        $item->setValor(0);
+        $this->assertFalse($item->itemValido());
+    }
+
+    public function testItemInvalidoQuandoDescricaoVazia() {
+        $descricao = '';
         $valor = 10;
-        $this->assertEquals($valor, 10);
+
+        $item = new Item();
+        $item->setValor($valor);
+        $item->setDescricao($descricao);
+        $this->assertFalse($item->itemValido());
+    }
+
+    public function testItemInvalido() {
+        $descricao = '';
+        $valor = 0;
+
+        $item = new Item();
+        $item->setValor($valor);
+        $item->setDescricao($descricao);
+        $this->assertFalse($item->itemValido());
     }
 }
