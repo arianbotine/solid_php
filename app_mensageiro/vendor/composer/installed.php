daae24844344fd904ba7102f98d57ebde6cf0c7e
Arian Botine <arian.botine@shift.com.br>
2023-03-07 23:33:24 -0300

    Iniciando projeto sobre inversão de dependencia

---
+++ app_mensageiro/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'arian.botine/app_mensageiro',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4955fbfc0ba38e2c317acc466afb7ec689280d85',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'arian.botine/app_mensageiro' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4955fbfc0ba38e2c317acc466afb7ec689280d85',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
